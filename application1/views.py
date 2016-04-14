c7adf2b7a5dc52de2dba9c8bef7680070df9e42b
elk1o <elk1o.dev@gmail.com>
2016-04-14 18:21:17 +0200

    Setting up on virtualenv

--- application1/views.py
+++ application1/views.py
@@ -2,14 +2,15 @@
 from django.shortcuts import render               #Renderización genérica
 from django.http import HttpResponse              #Importar plantilla de respuestas automáticas de las páginas con protocolo http
 from django.shortcuts import render_to_response   #Renderización de la respuesta
-from starting_django import settings              #Importar configuracion del fichero settings.py
+from django.conf import settings              #Importar configuracion del fichero settings.py
 from django.http import HttpResponseRedirect      #Importar respuestas directas al protocolo http (Páginas al vuelo: Ej: cad = "<html>[...]<h1>hola!</h1>[...]</html>")
 from django.template import RequestContext        #Comunicación M+C con V
 from application1.models import Categoria, Espectaculo, Lugar
 
 # Create your views here.
 def index(request):
-    print(settings.TEMPLATE_DIRS)
+    print(settings.RUTA_PROYECTO)
+    print(settings.PROJECT_ROOT)
     categories = Categoria.objects.all()
     return render_to_response('index.html', {'categories': categories})
 
