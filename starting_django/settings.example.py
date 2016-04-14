38c27f7f7ab637e10f38a5053db77adc5a3d9de9
elk1o <elk1o.dev@gmail.com>
2016-04-14 11:47:17 +0200

    Fix second settings

--- starting_django/settings.example.py
+++ starting_django/settings.example.py
@@ -91,23 +91,14 @@ WSGI_APPLICATION = 'starting_django.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'pruebas_python',
-        'USER': 'root',
-        'PASSWORD': 'ioQwscQS',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
-'''
-'ENGINE': 'django.db.backends.sqlite3',
-'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-'ENGINE': 'django.db.backends.mysql',
-'NAME': 'testpython',
-'USER': 'root',
-'PASSWORD': 'ioQwscQS',
-'HOST': '127.0.0.1',
-'PORT': '3306',
-'''
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
